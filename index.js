077d4c90c2480e24c8d0b1707bf0cc18ed0724c3
Gabryel Borges <gabryelhenborges@gmail.com>
2025-07-11 16:24:06 -0300

    feat: Pegando dados do cacheamento quando existente

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import BancoDadosExemplo from "./bancodadosexemplo.js";
+import { createClient } from "redis";
 
 const app = express();
 const porta = 7777;
+
+const redisClient = createClient({
+  url: "redis://localhost:6379",
+});
 
 const getPessoas = async () => {
   const intervalo = Math.random() * 4000 + 1000; //simulando tempo de resposta de 1 a 5 segundos
@@ -14,10 +19,19 @@ const getPessoas = async () => {
 };
 
 app.get("/", async (req, res) => {
-  const pessoas = await getPessoas();
+  const cachePessoas = await redisClient.get("key_pessoas");//pegando do cache Redis
+  if(cachePessoas){
+    return res.status(200).send(JSON.parse(cachePessoas));//se existir no cache, retornar os dados
+  }
+  const pessoas = await getPessoas();//se não há nada na cache, buscar no banco de dados
+  await redisClient.set("key_pessoas", JSON.stringify(pessoas));//armazenando no redis
   res.status(200).send(pessoas);
 });
 
-app.listen(porta, () => {
-  console.log(`Servidor rodando na porta ${porta}`);
-});
+const startup = async () => {
+  await redisClient.connect();
+  app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+  });
+};
+startup();

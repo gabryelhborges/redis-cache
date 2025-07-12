294c780ac271f068fc59a2780ff936c399e6cb0c
Gabryel Borges <gabryelhenborges@gmail.com>
2025-07-11 22:48:09 -0300

    feat: Adicionando lógica de exclusão da cache quando ela estiver desatualizada

--- index.js
+++ index.js
@@ -18,13 +18,21 @@ const getPessoas = async () => {
   });
 };
 
+// rota GET para simular que foi feita alguma alteração na base de dados
+app.get("/alterar-pessoa", async (req, res) => {
+  // algum cliente foi alterado, logo a cache está desatualizada
+  await redisClient.del("key_pessoas"); //deletando o cache
+  res.status(200).send("Pessoa 'alterada'");
+});
+
 app.get("/", async (req, res) => {
-  const cachePessoas = await redisClient.get("key_pessoas");//pegando do cache Redis
-  if(cachePessoas){
-    return res.status(200).send(JSON.parse(cachePessoas));//se existir no cache, retornar os dados
+  const cachePessoas = await redisClient.get("key_pessoas"); //pegando do cache Redis
+  if (cachePessoas) {
+    return res.status(200).send(JSON.parse(cachePessoas)); //se existir no cache, retornar os dados
   }
-  const pessoas = await getPessoas();//se não há nada na cache, buscar no banco de dados
-  await redisClient.set("key_pessoas", JSON.stringify(pessoas));//armazenando no redis
+  const pessoas = await getPessoas(); //se não há nada na cache, buscar no banco de dados
+  await redisClient.set("key_pessoas", JSON.stringify(pessoas)); //armazenando no redis
+  //await redisClient.set("key_pessoas", JSON.stringify(pessoas), { EX: 60 });//armazenando no redis com expiração de 60 segundos
   res.status(200).send(pessoas);
 });
 
@@ -35,3 +43,6 @@ const startup = async () => {
   });
 };
 startup();
+
+//para usar cacheamento é necessário entender onde está o problema, qual operação está consumindo mais recursos
+//apos isso, será possivel entender que tipo de cache será mais favorável para solucionar isso
